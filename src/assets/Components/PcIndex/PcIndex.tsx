837add104ab939120a270e5681013f7d97ffc835
mobin <mobinmohammadi4321@gmail.com>
2025-11-13 14:04:56 +0330

    firstComment

---
+++ src/assets/Components/PcIndex/PcIndex.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RightContent from "./RightContent/RightContent";
+import LeftContent from "./LeftContent/LeftContent";
+
+export default function PcIndex() {
+  return (
+    <div>
+      <RightContent />
+      <LeftContent />
+    </div>
+  );
+}
---
+++ src/assets/Pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Index from '../../Components/Index/Index'
+
+export default function Home() {
+  return (
+    <div>
+        <Index/>
+    </div>
+  )
+}
