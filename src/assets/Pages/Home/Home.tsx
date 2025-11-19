ce3913746cacca01ab1476424d91ddeb30a9f2f0
mobin <mobinmohammadi4321@gmail.com>
2025-11-19 16:46:06 +0330

    udateuserInfosPage

--- src/assets/Pages/Home/Home.tsx
+++ src/assets/Pages/Home/Home.tsx
@@ -1,9 +1,14 @@
-import Index from '../../Components/Index/Index'
+import Index from "../../Components/Index/Index";
 
 export default function Home() {
   return (
-    <div className='container-custom pt-4' >
-        <Index/>
+    <div className="container-custom pt-4">
+      <Index />
+      <div className="bg-secondry fixed bottom-5 flex items-center justify-center right-5 w-8 h-8 rounded-full">
+        <svg className="w-5 h-5 mr-0.5">
+          <use xlinkHref="#support"></use>
+        </svg>
+      </div>
     </div>
-  )
+  );
 }
